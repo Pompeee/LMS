45545eacb2eeabcaea4889a0408d02733d71f938
Pontus <56864880+Pompeee@users.noreply.github.com>
2020-01-24 12:23:54 +0100

    Student

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,4 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Student {
+
+    private String studentName;
+    private String regNum;
+
+    Book borrowedBooks[] = new Book[3];
+    public int bookCount = 0;
+
+    Scanner input = new Scanner(System.in);
+
+    public Student() {
+        System.out.println("Enter Student Name:");
+        this.studentName = input.nextLine();
+    }
 }
