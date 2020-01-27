ff5cf7d6bca0c94a9625ab8c9a9dc04ef099edb0
Pontus <56864880+Pompeee@users.noreply.github.com>
2020-01-27 15:45:18 +0100

    Books: copareBooksObject method

--- src/com/company/Books.java
+++ src/com/company/Books.java
@@ -1,4 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Books {
+
+    Book theBook[] = new Book[50];
+    private static int count;
+
+    Scanner input = new Scanner(System.in);
+
+    public int compareBookObject(Book b1, Book b2) {
+        if (b1.bookName.equalsIgnoreCase(b2.bookName)) {
+
+            System.out.println("The book of this name already exists");
+            return 0;
+        }
+        return 0;
+    }
 }
