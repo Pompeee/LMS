0731aaa14b3310ce4d40585a8ffc49c8ead8ce29
Pontus <56864880+Pompeee@users.noreply.github.com>
2020-01-27 16:45:26 +0100

    Books: isAvailable method

--- src/com/company/Books.java
+++ src/com/company/Books.java
@@ -119,6 +119,41 @@ public class Books {
         }
     }
 
+    public void dipMenu() {
+
+        System.out.println("----------------------------------------------------------------------------------------------------------");
+        System.out.println("Enter 0 to Exit Application.");
+        System.out.println("Enter 1 to Add new Book.");
+        System.out.println("Enter 2 to Upgrade Quantity of a Book.");
+        System.out.println("Enter 3 to Search a Book.");
+        System.out.println("Enter 4 to Show All Books.");
+        System.out.println("Enter 5 to Register Student.");
+        System.out.println("Enter 6 to Show All Registered Students.");
+        System.out.println("Enter 7 to Check Out Book. ");
+        System.out.println("Enter 8 to Check In Book");
+        System.out.println("----------------------------------------------------------------------------------------------------------");
+
+    }
+
+    public int isAvailable(int sNo) {
+        for (int i = 0; i < count; i++) {
+
+            if (sNo == theBooks[i].sNo) {
+                if (theBooks[i].bookQtyCopy > 0) {
+
+                    System.out.println("Book is available");
+                    return i;
+                }
+                System.out.println("Book is unavailable");
+                return -1;
+            }
+        }
+
+        System.out.println("No Book of Serial Number " + " Available in Library.");
+        return -1;
+
+    }
+
 
 
 }
